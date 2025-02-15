59bc86fee92d13736f4a027932fd6363d96d41c1
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-02-15 20:53:19 +0400

    first commit

---
+++ src/main/java/az/edu/turing/module1/lesson03/Car.java
@@ -0,0 +1,13 @@
+package az.edu.turing.module1.lesson03;
+
+public class Car {
+
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }}
