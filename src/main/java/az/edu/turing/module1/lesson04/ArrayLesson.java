033e4e1ee5f23e66c88dbd766dd727024f46242a
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-02-16 19:46:33 +0400

    new commit

---
+++ src/main/java/az/edu/turing/module1/lesson04/ArrayLesson.java
@@ -0,0 +1,27 @@
+package az.edu.turing.module1.lesson04;
+
+import java.util.Scanner;
+
+public class ArrayLesson {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter length of array:");
+        int length = scanner.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.print("Enter number: ");
+            arr[i] = scanner.nextInt();
+
+        }
+
+        int maxNumber = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxNumber) {
+                maxNumber = arr[i];
+            }
+        }
+        System.out.println(maxNumber);
+    }
+}
