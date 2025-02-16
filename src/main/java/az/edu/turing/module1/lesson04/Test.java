2eb85579b7c3656f15508e25992976c875ddf96b
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-02-16 19:28:58 +0400

    new commit

--- src/main/java/az/edu/turing/module1/lesson04/Test.java
+++ src/main/java/az/edu/turing/module1/lesson04/Test.java
@@ -1,8 +1,16 @@
 package az.edu.turing.module1.lesson04;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-
-
+        int[] arr = {1, 3, 5, 99, 101, 4, 12};
+        int maxNumber = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxNumber) {
+                maxNumber = arr[i];
+            }
+        }
+        System.out.println(maxNumber);
     }
 }
