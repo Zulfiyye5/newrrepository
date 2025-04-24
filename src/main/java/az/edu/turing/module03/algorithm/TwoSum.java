856454aa86f82691a1275d551503475e0f78429c
Zulfiyye <zulfiyyerehimova347@gmail.com>
2025-04-24 19:48:28 +0400

    Solution of Two Sum problem

---
+++ src/main/java/az/edu/turing/module03/algorithm/TwoSum.java
@@ -0,0 +1,32 @@
+package az.edu.turing.module03.algorithm;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int x= target-nums[i];
+
+            if(map.containsKey(x)){
+                res[0] = map.get(x);
+                res[1] = i;
+
+
+
+            }
+
+            map.put(nums[i],i);
+
+
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        twoSum(nums,9);
+    }
+}
